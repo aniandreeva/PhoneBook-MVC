7e23cb39d999deb350334d09bae66e616bb348e2
Anita A <Anita A>
2016-04-15 14:53:16 +0300

    Add CountriesRepository and CitiesRepository

---
+++ PhoneBook/Repositories/CitiesRepository.cs
@@ -0,0 +1,15 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Repositories
+{
+    public class CitiesRepository:BaseRepository<City>
+    {
+        public CitiesRepository() : base() { }
+
+        public CitiesRepository(UnitOfWork unitOfWork) : base(unitOfWork) { }
+    }
+}
---
+++ PhoneBook/Repositories/CountriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Repositories
+{
+    public class CountriesRepository: BaseRepository<Country>
+    {
+        public CountriesRepository() : base() { }
+
+        public CountriesRepository(UnitOfWork unitOfWork):base(unitOfWork){ }
+    }
+}
